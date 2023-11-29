a6d0b195ecda7747ef398c8573baaf81552b077c
karanara <karanara@mail.uc.edu>
2023-11-29 01:07:20 -0500

    configured through applicaation properities for values in drop down

--- src/main/java/com/practice/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/practice/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.practice.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,15 @@ import com.practice.springboot.thymeleafdemo.model.Student;
 
 @Controller
 public class StudentController {
-
+ 
+    @Value("${countries}")
+	private List<String> countries;
  
 	@GetMapping("/showStudentform")
 	public String getStudentDetails(Model theModel) {
 		Student theStudent = new Student();
 		theModel.addAttribute("student", theStudent);
+		theModel.addAttribute("countries", countries);
 		return "student-form";
 		
 	}
